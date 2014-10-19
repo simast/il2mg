5d8becac4037751452a4fa69c341bc7545920343
Simas Toleikis <simast@gmail.com>
2014-10-19 10:27:07 +0300

    Draw airfield icons on the map in debug mode.

--- src/make/airfields.js
+++ src/make/airfields.js
@@ -1,7 +1,41 @@
 /** @copyright Simas Toleikis, 2014 */
 "use strict";
 
+var DATA = require("../mission").DATA;
+var Block = require("../block");
+
 // Generate mission airfields
 module.exports = function(mission) {
 
+	var params = mission.params;
+	var battle = mission.battle;
+
+	// Draw airfield icons on the map in debug mode
+	if (params.debug) {
+
+		// Make a list of active battle coalitions
+		var activeCoalitions = [];
+
+		// Unknown coalition
+		activeCoalitions.push(0);
+
+		// Coalitions from active countries
+		battle.countries.forEach(function(countryID) {
+			activeCoalitions.push(DATA.countries[countryID].coalition);
+		});
+
+		for (var airfieldID in battle.airfields) {
+
+			var airfield = battle.airfields[airfieldID];
+
+			var airfieldIcon = new Block("MCU_Icon");
+
+			airfieldIcon.IconId = 903;
+			airfieldIcon.setPosition(airfield.position);
+			airfieldIcon.setCoalitions(activeCoalitions);
+			airfieldIcon.setName(mission.getLC(airfield.name));
+
+			mission.blocks.push(airfieldIcon);
+		}
+	}
 };
