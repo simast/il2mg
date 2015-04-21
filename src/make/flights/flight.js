c289c4e0b91bf49fec5b9b929dd5928d1a2c840c
Simas Toleikis <simast@gmail.com>
2015-04-21 08:48:13 +0300

    Added some more soviet units for testing.

--- src/make/flights/flight.js
+++ src/make/flights/flight.js
@@ -5,17 +5,17 @@ var Item = require("../../item");
 
 // Make mission flight
 module.exports = function makeFlight(params) {
-	
+
 	// TODO: params.player - player flight flag (or player plane ID)
 	// TODO: params.mission - mission ID
 	// TODO: params.unit - unit ID
 	// TODO: params.country - unit country ID
 	// TODO: params.planes - number of planes
 	// TODO: params.state - state (parked, runway, in progress, etc)
-	
+
 	var rand = this.rand;
 	var units = Object.keys(this.unitsByID);
-	
+
 	// Validate unit ID
 	if (params.unit && !this.unitsByID[params.unit]) {
 		throw new TypeError("Invalid flight unit ID value.");
@@ -24,16 +24,16 @@ module.exports = function makeFlight(params) {
 	if (!params.unit) {
 		params.unit = rand.pick(units);
 	}
-	
+
 	// Resolve unit groups
 	while (Array.isArray(this.unitsByID[params.unit])) {
 		params.unit = rand.pick(this.unitsByID[params.unit]);
 	}
-	
+
 	var unit = this.unitsByID[params.unit];
 	var plane = this.planesByID[rand.pick(unit.planes)];
 	var airfield = this.airfieldsByID[unit.airfield];
-	
+
 	var Plane = Item.Plane;
 	var planeObject = this.createItem("Plane");
 
@@ -44,7 +44,7 @@ module.exports = function makeFlight(params) {
 	planeObject.Model = plane.model;
 	planeObject.Country = unit.country;
 	planeObject.StartInAir = Plane.START_PARKING;
-	
+
 	if (params.player) {
 		planeObject.AILevel = Plane.AI_PLAYER;
 	}
