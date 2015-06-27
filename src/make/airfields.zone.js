191e0848508933e7560472b975b491efb47e8310
Simas Toleikis <simast@gmail.com>
2015-06-27 11:20:36 +0300

    New version of airfield zone unload bubble.

--- src/make/airfields.zone.js
+++ src/make/airfields.zone.js
@@ -1,8 +1,8 @@
 /** @copyright Simas Toleikis, 2015 */
 "use strict";
 
-// Max supported plane count in zone area
-var ZONE_MAX_PLANES = 6;
+// Time to delay the unloading of airfield zone area
+var ZONE_UNLOAD_TIME = 60 * 5; // 5 minutes
 
 // Make airfield zone ("bubble" activity trigger)
 module.exports = function makeAirfieldZone(airfield) {
@@ -19,24 +19,16 @@ module.exports = function makeAirfieldZone(airfield) {
 	// Activity zone items
 	var zoneInitialize = zoneGroup.createItem("MCU_TR_MissionBegin");
 	var zoneTrigger = zoneGroup.createItem("MCU_TR_ComplexTrigger");
-	var zoneCounter = zoneGroup.createItem("MCU_Counter");
 	var zonePlus = zoneGroup.createItem("MCU_Timer");
 	var zoneMinus = zoneGroup.createItem("MCU_Timer");
-	var zoneMinusActivate = zoneGroup.createItem("MCU_Activate");
-	var zoneMinusDeactivate = zoneGroup.createItem("MCU_Deactivate");
-	var zoneMinusCounter = zoneGroup.createItem("MCU_Counter");
-	var zoneMinusFirst = zoneGroup.createItem("MCU_Timer");
-	var zoneMinusTick1 = zoneGroup.createItem("MCU_Timer");
-	var zoneMinusTick2 = zoneGroup.createItem("MCU_Timer");
-	var zoneMinusLast = zoneGroup.createItem("MCU_Timer");
-	var zoneMinusCounterActivate = zoneGroup.createItem("MCU_Activate");
-	var zoneMinusCounterDeactivate = zoneGroup.createItem("MCU_Deactivate");
 	var zoneLoad = zoneGroup.createItem("MCU_Timer");
-	var zoneLoadDeactivate = zoneGroup.createItem("MCU_Deactivate");
 	var zoneLoadActivate = zoneGroup.createItem("MCU_Activate");
+	var zoneLoadDeactivate = zoneGroup.createItem("MCU_Deactivate");
 	var zoneActivate = zoneGroup.createItem("MCU_Activate");
 	var zoneUnload = zoneGroup.createItem("MCU_Timer");
 	var zoneUnloadActivate = zoneGroup.createItem("MCU_Activate");
+	var zoneUnloadDeactivate = zoneGroup.createItem("MCU_Deactivate");
+	var zoneUnloadActivateTimer = zoneGroup.createItem("MCU_Timer");
 	var zoneDeactivate = zoneGroup.createItem("MCU_Deactivate");
 
 	zoneTrigger.setPosition(airfield.position);
@@ -48,21 +40,21 @@ module.exports = function makeAirfieldZone(airfield) {
 	zoneTrigger.EventsFilterEnteredSimple = 1;
 	zoneTrigger.EventsFilterLeftSimple = 1;
 	zoneTrigger.EventsFilterFinishedSimple = 1;
+	zoneTrigger.EventsFilterStationaryAndAlive = 1;
+	zoneTrigger.EventsFilterTookOff = 1;
 
-	// Map trigger events to zone +1 and -1 actions
+	// Map trigger events to zone plus and minus actions
 	zoneTrigger.addEvent("OnObjectSpawned", zonePlus);
 	zoneTrigger.addEvent("OnObjectEntered", zonePlus);
+	zoneTrigger.addEvent("OnObjectStationaryAndAlive", zonePlus);
+	zoneTrigger.addEvent("OnObjectTookOff", zonePlus);
 	zoneTrigger.addEvent("OnObjectLeft", zoneMinus);
 	zoneTrigger.addEvent("OnObjectFinished", zoneMinus);
-	
-	// Setup main zone entity counter
-	zoneCounter.Counter = ZONE_MAX_PLANES + 1;
-	zoneCounter.Dropcount = 1;
-	zoneCounter.addTarget(zoneUnload);
 
-	// Zone plus entity action will directly trigger zone load event
+	// Zone plus actions (trigger load and deactivate unload event)
 	zonePlus.addTarget(zoneLoad);
-	zonePlus.addTarget(zoneCounter);
+	zonePlus.addTarget(zoneUnloadDeactivate);
+	zoneUnloadDeactivate.addTarget(zoneUnload);
 
 	// Deactivate zone load event on first use
 	zoneLoad.addTarget(zoneLoadDeactivate);
@@ -76,72 +68,28 @@ module.exports = function makeAirfieldZone(airfield) {
 	zoneLoad.addTarget(zoneActivate);
 	zoneUnload.addTarget(zoneDeactivate);
 
-	// Setup zone minus target links
-	zoneMinus.addTarget(zoneMinusDeactivate);
-	zoneMinus.addTarget(zoneMinusCounterActivate);
-	zoneMinus.addTarget(zoneMinusFirst);
-
-	// Deactivate (freeze) zone minus/plus/unload events when entering decrement logic
-	zoneMinusDeactivate.addTarget(zoneMinus);
-	zoneMinusDeactivate.addTarget(zoneUnload);
-	zoneMinusDeactivate.addTarget(zonePlus);
-
-	// Activate (unfreeze) zone minus/plus events when leaving decrement logic
-	zoneMinusActivate.addTarget(zoneMinus);
-	zoneMinusActivate.addTarget(zonePlus);
-	
-	// Setup zone decrement counter
-	zoneMinusCounter.Counter = ZONE_MAX_PLANES - 2;
-	zoneMinusCounter.Dropcount = 1;
-	zoneMinusCounter.addTarget(zoneMinusCounterDeactivate);
-	zoneMinusCounter.addTarget(zoneMinusLast);
-	zoneMinusCounter.addTarget(zoneUnloadActivate);
-
-	// Setup zone decrement counter timers
-	zoneMinusFirst.Time = 0.001; // 1ms
-	zoneMinusTick1.Time = 0.02; // 20ms
-	zoneMinusTick2.Time = 0.02; // 20ms
-	zoneMinusLast.Time = 0.02; // 20ms
-	zoneMinusFirst.addTarget(zoneCounter);
-	zoneMinusFirst.addTarget(zoneMinusTick1);
-	zoneMinusTick1.addTarget(zoneCounter);
-	zoneMinusTick1.addTarget(zoneMinusTick2);
-	zoneMinusTick1.addTarget(zoneMinusCounter);
-	zoneMinusTick2.addTarget(zoneCounter);
-	zoneMinusTick2.addTarget(zoneMinusTick1);
-	zoneMinusTick2.addTarget(zoneMinusCounter);
-	zoneMinusLast.addTarget(zoneCounter);
-	zoneMinusLast.addTarget(zoneMinusActivate);
-	
-	// Setup zone decrement counter activation/deactivation
-	zoneMinusCounterActivate.addTarget(zoneMinusTick1);
-	zoneMinusCounterActivate.addTarget(zoneMinusTick2);
-	zoneMinusCounterDeactivate.addTarget(zoneMinusTick1);
-	zoneMinusCounterDeactivate.addTarget(zoneMinusTick2);
-	
-	// Reactivate unload event after decrement counter logic
+	// Zone minus actions (activate unload event and shedulle delayed unload action)
+	zoneMinus.addTarget(zoneUnloadActivate);
+	zoneMinus.addTarget(zoneUnloadActivateTimer);
+	zoneUnloadActivateTimer.Time = 0.001; // 1ms
 	zoneUnloadActivate.addTarget(zoneUnload);
+	zoneUnloadActivateTimer.addTarget(zoneUnload);
+	
+	// Delay time for unload action
+	zoneUnload.Time = ZONE_UNLOAD_TIME;
 
 	// Position all other zone items near the trigger
 	zoneInitialize.setPositionNear(zoneTrigger);
-	zoneCounter.setPositionNear(zoneTrigger);
 	zonePlus.setPositionNear(zoneTrigger);
 	zoneMinus.setPositionNear(zoneTrigger);
-	zoneMinusActivate.setPositionNear(zoneTrigger);
-	zoneMinusDeactivate.setPositionNear(zoneTrigger);
-	zoneMinusCounter.setPositionNear(zoneTrigger);
-	zoneMinusFirst.setPositionNear(zoneTrigger);
-	zoneMinusTick1.setPositionNear(zoneTrigger);
-	zoneMinusTick2.setPositionNear(zoneTrigger);
-	zoneMinusLast.setPositionNear(zoneTrigger);
-	zoneMinusCounterActivate.setPositionNear(zoneTrigger);
-	zoneMinusCounterDeactivate.setPositionNear(zoneTrigger);
 	zoneLoad.setPositionNear(zoneTrigger);
-	zoneLoadDeactivate.setPositionNear(zoneTrigger);
 	zoneLoadActivate.setPositionNear(zoneTrigger);
+	zoneLoadDeactivate.setPositionNear(zoneTrigger);
 	zoneActivate.setPositionNear(zoneTrigger);
 	zoneUnload.setPositionNear(zoneTrigger);
 	zoneUnloadActivate.setPositionNear(zoneTrigger);
+	zoneUnloadDeactivate.setPositionNear(zoneTrigger);
+	zoneUnloadActivateTimer.setPositionNear(zoneTrigger);
 	zoneDeactivate.setPositionNear(zoneTrigger);
 	
 	// Expose public zone event items
