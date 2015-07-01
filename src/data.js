0c7b54428babca8f8cbad54ba0f4b9f750ec43f0
Simas Toleikis <simas.toleikis@adform.com>
2015-07-01 12:18:23 +0300

    Refactored some data files.

--- src/data.js
+++ src/data.js
@@ -5,8 +5,77 @@ var fs = require("fs");
 var requireDir = require("require-directory");
 var stripJSONComments = require("strip-json-comments");
 
-// Get/load all static data
-var DATA = (function() {
+var DATA = Object.create(null);
+
+// Application name
+DATA.name = "il2mg";
+
+// Application version
+DATA.version = "r1";
+
+// List of supported mission localization languages
+DATA.languages = Object.freeze([
+	"eng" // English
+]);
+
+// Data tags for special airfield items
+DATA.itemTag = Object.freeze({
+	PLANE: -1, // Plane spot
+	TRUCK_CARGO: -2, // Cargo truck
+	TRUCK_FUEL: -3, // Fuel truck
+	CAR: -4, // Car vehicle
+	AA_MG: -5, // Anti-aircraft (MG)
+	AA_FLAK: -6, // Anti-aircraft (Flak)
+	LIGHT_SEARCH: -7, // Search light
+	LIGHT_LAND: -8, // Landing light
+	BEACON: -9, // Beacon
+	WINDSOCK: -10, // Windsock
+	EFFECT: -11, // Effect
+	WRECK: -12 // Wreckage
+});
+
+// Data flags for airfield items
+DATA.itemFlag = Object.freeze({
+	BLOCK_DECO: 1, // Decoration
+	BLOCK_FUEL: 2, // Fuel item
+	PLANE_CAMO: 1, // Camouflage plane spot
+	EFFECT_SMOKE: 1, // House smoke effect
+	EFFECT_CAMP: 2, // Campfire effect
+	EFFECT_LAND: 3, // Landing fire effect
+	EFFECT_SIREN: 4, // Siren effect
+	TAXI_INV: 1, // Invertible taxi route
+	TAXI_RUNWAY: 1, // Taxi runway point
+	ROUTE_STOP: 1, // Route stop point
+	ROUTE_ROAD: 2 // Route road formation
+});
+
+// Plane size constants (types/IDs)
+DATA.planeSize = Object.freeze({
+	SMALL: 1,
+	MEDIUM: 2,
+	LARGE: 3,
+	HUGE: 4
+});
+
+// Flight states
+// NOTE: Numeric (0..1) flight states represent aircraft in the air at various mission states
+DATA.flightState = Object.freeze({
+	START: "start", // Parking, engine not running
+	TAXI: "taxi", // On the taxiway, engine running, taxiing to runway
+	RUNWAY: "runway" // On the runway, engine running, ready for takeoff
+});
+
+// Flight plan actions
+DATA.planAction = Object.freeze({
+	START: "start", // Initial start action
+	TAKEOFF: "takeoff", // Taxi (optionally) and takeoff from airfield
+	WAIT: "wait", // Wait for something (do nothing)
+	FLY: "fly", // Fly to waypoint/location
+	LAND: "land" // Land on airfield (ending the flight)
+});
+
+// Load all static data
+(function() {
 
 	var Module = require("module");
 	var origJSONLoader = Module._extensions[".json"];
@@ -20,27 +89,22 @@ var DATA = (function() {
 		module._compile(js, filename);
 	};
 
-	var data = Object.create(null);
-
-	data.name = require("../data/name");
-	data.version = require("../data/version");
-	data.vehicles = Object.freeze(require("../data/vehicles"));
-	data.clouds = Object.freeze(require("../data/clouds"));
-	data.coalitions = Object.freeze(require("../data/coalitions"));
-	data.languages = Object.freeze(require("../data/languages"));
-	data.time = Object.freeze(require("../data/time"));
-	data.items = Object.freeze(require("../data/items"));
-	data.effects = Object.freeze(require("../data/effects"));
-	data.grounds = Object.freeze(require("../data/grounds"));
-	data.callsigns = Object.freeze(require("../data/callsigns"));
-	data.countries = Object.freeze(require("../data/countries"));
-	data.battles = Object.freeze(require("../data/battles"));
-	data.planes = Object.create(null);
+	DATA.vehicles = Object.freeze(require("../data/vehicles"));
+	DATA.clouds = Object.freeze(require("../data/clouds"));
+	DATA.coalitions = Object.freeze(require("../data/coalitions"));
+	DATA.time = Object.freeze(require("../data/time"));
+	DATA.items = Object.freeze(require("../data/items"));
+	DATA.effects = Object.freeze(require("../data/effects"));
+	DATA.grounds = Object.freeze(require("../data/grounds"));
+	DATA.callsigns = Object.freeze(require("../data/callsigns"));
+	DATA.countries = Object.freeze(require("../data/countries"));
+	DATA.battles = Object.freeze(require("../data/battles"));
+	DATA.planes = Object.create(null);
 
 	// Load country info
-	for (var countryID in data.countries) {
+	for (var countryID in DATA.countries) {
 
-		var country = data.countries[countryID];
+		var country = DATA.countries[countryID];
 		var countryPath = "../data/countries/" + countryID + "/";
 
 		country.ranks = require(countryPath + "ranks");
@@ -52,18 +116,20 @@ var DATA = (function() {
 	
 	for (var planeGroup in planeData) {
 		for (var planeID in planeData[planeGroup]) {
-			data.planes[planeID] = planeData[planeGroup][planeID];
+			DATA.planes[planeID] = planeData[planeGroup][planeID];
 		}
 	}
+	
+	Object.freeze(DATA.planes);
 
 	// Load battle info
 	// TODO: Load only required mission battle data
-	for (var battleID in data.battles) {
+	for (var battleID in DATA.battles) {
 
-		var battle = data.battles[battleID];
+		var battle = DATA.battles[battleID];
 		var battlePath = "../data/battles/" + battleID + "/";
 
-		battle.countries = Object.freeze(require(battlePath + "countries"));
+		battle.countries = [];
 		battle.blocks = Object.freeze(require(battlePath + "blocks"));
 		battle.fronts = Object.freeze(require(battlePath + "fronts"));
 		battle.map = Object.freeze(require(battlePath + "map"));
@@ -71,33 +137,33 @@ var DATA = (function() {
 		battle.weather = Object.freeze(require(battlePath + "weather"));
 		battle.airfields = requireDir(module, battlePath + "airfields");
 		battle.units = Object.create(null);
-
-		// Load country-specific battle units
-		for (var unitCountryID of battle.countries) {
+		
+		// Load battle unit data
+		var unitsData = requireDir(module, battlePath + "units/");
+		
+		for (var unitCountryID in unitsData) {
 			
-			var unitData;
-
-			try {
-				unitData = requireDir(module, battlePath + "units/" + unitCountryID);
-			}
-			catch (e) {
-				continue;
-			}
+			var unitsDataCountry = unitsData[unitCountryID];
 			
-			for (var unitGroup in unitData) {
-				for (var unitID in unitData[unitGroup]) {
+			// Build units list
+			for (var unitGroup in unitsDataCountry) {
+				for (var unitID in unitsDataCountry[unitGroup]) {
 					
-					battle.units[unitID] = unitData[unitGroup][unitID];
+					battle.units[unitID] = unitsDataCountry[unitGroup][unitID];
 					battle.units[unitID].country = unitCountryID;
 				}
 			}
+			
+			// Register country as part of the battle
+			battle.countries.push(unitCountryID);
 		}
+		
+		Object.freeze(battle.countries);
+		Object.freeze(battle.units);
 	}
 
 	// Restore original Node JSON loader
 	Module._extensions[".json"] = origJSONLoader;
-
-	return data;
 }());
 
 /**
@@ -171,61 +237,5 @@ DATA.getItemType = function(itemTypeID) {
 
 	return itemType;
 };
-
-// Data tags for special airfield items
-DATA.itemTag = Object.freeze({
-	PLANE: -1, // Plane spot
-	TRUCK_CARGO: -2, // Cargo truck
-	TRUCK_FUEL: -3, // Fuel truck
-	CAR: -4, // Car vehicle
-	AA_MG: -5, // Anti-aircraft (MG)
-	AA_FLAK: -6, // Anti-aircraft (Flak)
-	LIGHT_SEARCH: -7, // Search light
-	LIGHT_LAND: -8, // Landing light
-	BEACON: -9, // Beacon
-	WINDSOCK: -10, // Windsock
-	EFFECT: -11, // Effect
-	WRECK: -12 // Wreckage
-});
-
-// Data flags for airfield items
-DATA.itemFlag = Object.freeze({
-	BLOCK_DECO: 1, // Decoration
-	BLOCK_FUEL: 2, // Fuel item
-	PLANE_CAMO: 1, // Camouflage plane spot
-	EFFECT_SMOKE: 1, // House smoke effect
-	EFFECT_CAMP: 2, // Campfire effect
-	EFFECT_LAND: 3, // Landing fire effect
-	EFFECT_SIREN: 4, // Siren effect
-	TAXI_INV: 1, // Invertible taxi route
-	TAXI_RUNWAY: 1, // Taxi runway point
-	ROUTE_STOP: 1, // Route stop point
-	ROUTE_ROAD: 2 // Route road formation
-});
-
-// Plane size constants (types/IDs)
-DATA.planeSize = Object.freeze({
-	SMALL: 1,
-	MEDIUM: 2,
-	LARGE: 3,
-	HUGE: 4
-});
-
-// Flight states
-// NOTE: Numeric (0..1) flight states represent aircraft in the air at various mission states
-DATA.flightState = Object.freeze({
-	START: "start", // Parking, engine not running
-	TAXI: "taxi", // On the taxiway, engine running, taxiing to runway
-	RUNWAY: "runway" // On the runway, engine running, ready for takeoff
-});
-
-// Flight plan actions
-DATA.planAction = Object.freeze({
-	START: "start", // Initial start action
-	TAKEOFF: "takeoff", // Taxi (optionally) and takeoff from airfield
-	WAIT: "wait", // Wait for something (do nothing)
-	FLY: "fly", // Fly to waypoint/location
-	LAND: "land" // Land on airfield (ending the flight)
-});
 
 global.DATA = DATA;
