d912468e058adc427018764938e7cf494ceb6956
Simas Toleikis <simast@gmail.com>
2015-11-11 23:13:05 +0200

    Updated some front lines (around Tatsinskaya raid).

--- src/data.js
+++ src/data.js
@@ -87,7 +87,7 @@ DATA.planAction = Object.freeze({
 
 // Map colors as RGB array values
 DATA.mapColor = Object.freeze({
-	ATTACK: [148, 148, 148]
+	ATTACK: [156, 156, 156]
 });
 
 // Briefing colors as HTML hex color values
