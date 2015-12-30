091d72dfa1d8d91723c73c8223f20ccf8838aaf3
Simas Toleikis <simast@gmail.com>
2015-12-30 19:01:41 +0200

    Tweaks to free flight plan.

--- src/make/task.free.js
+++ src/make/task.free.js
@@ -9,10 +9,23 @@ module.exports = function makeTaskFree(flight) {
 	
 	const rand = this.rand;
 	const plan = flight.plan;
+	const isPlayerFlightLeader = (flight.player === flight.leader);
 	
-	// FIXME: Random wait action
+	// Skip wait and land plan actions where player is leader of the flight
+	if (isPlayerFlightLeader) {
+		
+		flight.plan.push({
+			makeAction: (action, element, flight, input) => {
+				plan.land.makeAction = false;
+			}
+		});
+		
+		return;
+	}
+	
+	// Use wait (followed by land) action for other flights
 	plan.push({
 		type: planAction.WAIT,
-		time: rand.integer(60, 90)
+		time: rand.integer(15 * 60, 30 * 60) // 15-30 minutes
 	});
 };
