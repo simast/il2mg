0a3bf37a1fb9eb36ecf1a5a636c5534f3606928e
Simas Toleikis <simast@gmail.com>
2016-01-23 13:22:38 +0200

    Added stub patrol task.

---
+++ src/make/task.patrol.js
@@ -0,0 +1,11 @@
+/** @copyright Simas Toleikis, 2016 */
+"use strict";
+
+// Data constants
+const planAction = DATA.planAction;
+
+// Make mission defensive patrol task
+module.exports = function makeTaskPatrol(flight) {
+	
+	// TODO:
+};
