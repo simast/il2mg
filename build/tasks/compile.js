78718d206294261b96e25c7c34bc67f3ee4615b3
Simas Toleikis <simas.toleikis@adform.com>
2016-01-28 10:49:40 +0200

    Updated enclose to 1.1.0.

--- build/tasks/compile.js
+++ build/tasks/compile.js
@@ -49,7 +49,7 @@ module.exports = function(grunt) {
 		const extension = (process.platform === "win32") ? ".exe" : "";
 		const binaryFilePath = "../" + buildName + extension;
 
-		options.push("--version", "5.4.0");
+		options.push("--version", "5.5.0");
 		options.push("--features", "no"); // Support all CPUs (don't use SSE3/AVX/etc)
 		options.push("--config", "../enclose.js");
 		options.push("--loglevel", "error");
