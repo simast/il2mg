11bf7babbd5439a23679c0e3d1cf308e682e82d1
Simas Toleikis <simas.toleikis@adform.com>
2016-03-14 17:21:13 +0200

    Minor tweaks to Electron/GUI integration.

--- gui/index.js
+++ gui/index.js
@@ -8,24 +8,46 @@ const BrowserWindow = electron.BrowserWindow;
 
 let mainWindow = null;
 
+// Make sure only a single app instance is allowed to run
+const isOtherInstance = app.makeSingleInstance(() => {
+	
+	if (mainWindow) {
+		
+		// Restore minimized main window
+		if (mainWindow.isMinimized()) {
+			mainWindow.restore();
+		}
+		
+		// Focus main window
+		mainWindow.focus();
+	}
+});
+
+if (isOtherInstance) {
+	
+	app.quit();
+	return;
+}
+
 // Quit when all windows are closed
 app.on("window-all-closed", () => {
 	app.quit();
 });
 
+// Create main application window
 app.on("ready", () => {
 	
-	// Create main window
 	mainWindow = new BrowserWindow({
 		title: app.getName(),
 		useContentSize: true,
 		width: 800,
-		height: 600,
 		minWidth: 800,
 		maxWidth: 800,
+		height: 600,
 		minHeight: 500,
 		maximizable: false,
-		acceptFirstMous: true,
+		acceptFirstMouse: true,
+		fullscreenable: false,
 		webPreferences: {
 			webgl: false,
 			webaudio: false,
