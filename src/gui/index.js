99f2b251131b5532a672230bdc500be388a43182
Simas Toleikis <simas.toleikis@adform.com>
2016-03-15 16:48:51 +0200

    Save and restore main window position/size.

--- src/gui/index.js
+++ src/gui/index.js
@@ -1,10 +1,10 @@
 /** @copyright Simas Toleikis, 2016 */
 "use strict";
 
-const electron = require("electron");
+const {app, BrowserWindow, hideInternalModules} = require("electron");
 
-const app = electron.app;
-const BrowserWindow = electron.BrowserWindow;
+// Disable legacy built-in module require style
+hideInternalModules();
 
 let mainWindow = null;
 
@@ -24,8 +24,20 @@ const isOtherInstance = app.makeSingleInstance(() => {
 });
 
 if (isOtherInstance) {
-	app.exit(0);
+	app.exit(0); // Success
 }
+
+const fs = require("fs");
+const path = require("path");
+
+// Default window size and constraints
+const WINDOW_WIDTH = 800;
+const WINDOW_HEIGHT = 600;
+const WINDOW_HEIGHT_MIN = 450;
+
+// Global JSON configuration data object
+const config = global.config = {};
+let configPath;
 
 // Quit when all windows are closed
 app.on("window-all-closed", () => {
@@ -35,14 +47,20 @@ app.on("window-all-closed", () => {
 // Create main application window
 app.on("ready", () => {
 	
-	mainWindow = new BrowserWindow({
+	// Load JSON configuration data
+	configPath = path.join(app.getPath("userData"), "config.json");
+	
+	try {
+		Object.assign(config, JSON.parse(fs.readFileSync(configPath, "utf-8")));
+	}
+	catch (e) {}
+	
+	const windowConfig = {
 		title: "il2mg - Mission Generator",
 		useContentSize: true,
-		width: 800,
-		minWidth: 800,
-		maxWidth: 800,
-		height: 600,
-		minHeight: 500,
+		minWidth: WINDOW_WIDTH,
+		maxWidth: WINDOW_WIDTH,
+		minHeight: WINDOW_HEIGHT_MIN,
 		maximizable: false,
 		acceptFirstMouse: true,
 		fullscreenable: false,
@@ -52,7 +70,25 @@ app.on("ready", () => {
 			plugins: false,
 			defaultEncoding: "UTF-8"
 		}
-	});
+	};
+	
+	// Use existing (saved) window size and position
+	if (config.window) {
+		
+		windowConfig.center = false;
+		windowConfig.x = config.window.x;
+		windowConfig.y = config.window.y;
+		windowConfig.width = config.window.width;
+		windowConfig.height = config.window.height;
+	}
+	// Use default window size and position
+	else {
+		
+		windowConfig.width = WINDOW_WIDTH;
+		windowConfig.height = WINDOW_HEIGHT;
+	}
+	
+	mainWindow = new BrowserWindow(windowConfig);
 	
 	mainWindow.setMenu(null);
 	mainWindow.loadURL("file://" + __dirname + "/index.html");
@@ -60,4 +96,25 @@ app.on("ready", () => {
 	mainWindow.on("closed", () => {
 		mainWindow = null;
 	});
+});
+
+// Write application configuration data
+app.on("before-quit", () => {
+	
+	if (!configPath || !mainWindow) {
+		return;
+	}
+	
+	const bounds = mainWindow.getBounds();
+	const contentSize = mainWindow.getContentSize();
+	
+	// Save main window position and content size
+	config.window = {
+		x: bounds.x,
+		y: bounds.y,
+		width: contentSize[0],
+		height: contentSize[1]
+	};
+	
+	fs.writeFileSync(configPath, JSON.stringify(config, null, "\t"));
 });
