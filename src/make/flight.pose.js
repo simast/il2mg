0cbf25f5733ab36dee65fc9a52493b8d778b906c
Simas Toleikis <simast@gmail.com>
2016-10-11 20:50:57 +0300

    Use active formation for flight pose.

--- src/make/flight.pose.js
+++ src/make/flight.pose.js
@@ -3,6 +3,7 @@
 
 const {Vector, Line} = require("sylvester");
 const {planAction} = require("../data");
+const {MCU_CMD_Formation} = require("../item");
 
 // Minimum and maximum pitch angle (for pose orientation)
 const MIN_PITCH_ANGLE = -15;
@@ -13,15 +14,23 @@ const AIRFIELD_SPAWN_MIN_ALT = 200;
 const AIRFIELD_SPAWN_MAX_ALT = 350;
 const AIRFIELD_SPAWN_RADIUS = 400;
 
+// Angle and spacing used for formation/plane positioning
+const FORMATION_ANGLE = 30; // Degrees
+const FORMATION_SPACING = 100; // Meters
+
 // Make flight air start pose (position and orientation based on formation)
 module.exports = function makeFlightPose(flight, fromPosition, toPosition) {
 	
+	const elements = flight.elements;
 	let spawnPosition;
 	let spawnOrientation;
-	let spawnVector;
-	let spawnRotationLine;
+	let spawnBackVector;
+	let spawnUpVector;
+	let spawnUpLine;
 	
-	for (const element of flight.elements) {
+	for (let e = 0; e < elements.length; e++) {
+		
+		const element = elements[e];
 		
 		// Apply pose only to elements with air start
 		if (typeof element.state !== "number") {
@@ -29,6 +38,8 @@ module.exports = function makeFlightPose(flight, fromPosition, toPosition) {
 		}
 		
 		const rand = this.rand;
+		const formation = element.formation;
+		let elementDistance = 0;
 		
 		// Find initial spawn position
 		if (!spawnPosition) {
@@ -105,42 +116,97 @@ module.exports = function makeFlightPose(flight, fromPosition, toPosition) {
 			spawnOrientation = [0, yaw, pitch]; // Roll, Yaw, Pitch
 			
 			// Inverted orientation vector (used for formation positioning)
-			spawnVector = Vector.create([diffX, diffY, diffZ])
+			spawnBackVector = Vector.create([diffX, diffY, diffZ])
 				.toUnitVector()
 				.multiply(-1);
 			
-			// Build a perpendicular spawn vector rotation axis line
-			pitch = (pitch + 90) % 360;
+			// Build a perpendicular spawn up vector and line
+			pitch = (pitch + 90) % 360 * (Math.PI / 180);
+			yaw = yaw * (Math.PI / 180);
 			
-			spawnRotationLine = Line.create(Vector.Zero(3), Vector.create([
-				Math.cos(yaw * (Math.PI / 180)) * Math.cos(pitch * (Math.PI / 180)),
-				Math.sin(pitch * (Math.PI / 180)),
-				Math.sin(yaw * (Math.PI / 180)) * Math.cos(pitch * (Math.PI / 180))
-			]));
+			spawnUpVector = Vector.create([
+				Math.cos(yaw) * Math.cos(pitch),
+				Math.sin(pitch),
+				Math.sin(yaw) * Math.cos(pitch)
+			]);
+			
+			spawnUpLine = Line.create(Vector.Zero(3), spawnUpVector);
 		}
 		
 		// Apply air start orientation and position for each plane
-		for (const plane of element) {
+		for (let planeIndex = 0; planeIndex < element.length; planeIndex++) {
 			
+			const plane = element[planeIndex];
 			const isElementLeader = (plane === element[0]);
 			const planeItem = plane.item;
+			let position = spawnPosition;
+			
+			// TODO: Support formation density
 			
 			// Set wingman position based on element formation
 			if (!isElementLeader) {
 				
-				spawnPosition = Vector.create(spawnPosition)
-					.add(spawnVector.multiply(80))
-					.rotate(Math.PI / 2, spawnRotationLine) // FIXME
-					.elements;
+				// Default line (none) formation
+				let distance = planeIndex * FORMATION_SPACING;
+				let angle = 0;
+				
+				// Left edge formation
+				if (formation === MCU_CMD_Formation.TYPE_PLANE_EDGE_LEFT) {
+					angle = -FORMATION_ANGLE;
+				}
+				// Right edge formation
+				else if (formation === MCU_CMD_Formation.TYPE_PLANE_EDGE_RIGHT) {
+					angle = FORMATION_ANGLE;
+				}
+				// V formation
+				else if (formation === MCU_CMD_Formation.TYPE_PLANE_V) {
+					
+					const direction = (planeIndex % 2 === 1) ? -1 : 1;
+					
+					distance = Math.ceil(planeIndex / 2) * FORMATION_SPACING;
+					angle = direction * FORMATION_ANGLE;
+				}
+				
+				// Add a small +-10% formation spacing and angle randomness
+				const spacingRand = FORMATION_SPACING * 0.1;
+				const angleRand = FORMATION_ANGLE * 0.1;
+				
+				distance += rand.real(-spacingRand, spacingRand, true);
+				angle += rand.real(-angleRand, angleRand, true);
+				
+				const planeVector = spawnBackVector
+					.multiply(distance)
+					.rotate(angle * (Math.PI / 180), spawnUpLine)
+					.add(spawnUpVector.multiply(distance * -0.03));
+				
+				position = Vector.create(spawnPosition).add(planeVector).elements;
+				elementDistance = Math.max(elementDistance, distance);
 			}
 			
 			planeItem.setOrientation(spawnOrientation);
-			planeItem.setPosition(spawnPosition);
+			planeItem.setPosition(position);
 		}
 		
-		// FIXME:
-		spawnPosition = Vector.create(spawnPosition)
-			.add(spawnVector.multiply(200))
-			.elements;
+		const isLastElement = (e + 1 === elements.length);
+		
+		// Set next element position
+		if (!isLastElement) {
+			
+			const angleBack = rand.real(-FORMATION_ANGLE, FORMATION_ANGLE, true);
+			const distanceRand = FORMATION_SPACING * 0.1;
+			let distanceBack = elementDistance + FORMATION_SPACING * 2;
+			let distanceUp = FORMATION_SPACING;
+			
+			// Add a small +- 10% element spacing randomness
+			distanceBack += rand.real(-distanceRand, distanceRand, true);
+			distanceUp += rand.real(-distanceRand, distanceRand, true);
+			
+			const elementVector = spawnBackVector
+				.multiply(distanceBack)
+				.rotate(angleBack * (Math.PI / 180), spawnUpLine)
+				.add(spawnUpVector.multiply(distanceUp));
+
+			spawnPosition = Vector.create(spawnPosition).add(elementVector).elements;
+		}
 	}
 };
