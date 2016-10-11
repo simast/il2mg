afdbb2d3c11478084216e08b0c873629b673f4d6
Simas Toleikis <simas.toleikis@adform.com>
2016-10-11 17:29:41 +0300

    Flight pose logic tweaks.

--- src/make/flight.pose.js
+++ src/make/flight.pose.js
@@ -1,12 +1,17 @@
 /** @copyright Simas Toleikis, 2016 */
 "use strict";
 
-const {Vector} = require("sylvester");
+const {Vector, Line} = require("sylvester");
 const {planAction} = require("../data");
 
 // Minimum and maximum pitch angle (for pose orientation)
 const MIN_PITCH_ANGLE = -15;
 const MAX_PITCH_ANGLE = +15;
+
+// Airfield spawn altitude and area radius constraints (in meters)
+const AIRFIELD_SPAWN_MIN_ALT = 200;
+const AIRFIELD_SPAWN_MAX_ALT = 350;
+const AIRFIELD_SPAWN_RADIUS = 400;
 
 // Make flight air start pose (position and orientation based on formation)
 module.exports = function makeFlightPose(flight, fromPosition, toPosition) {
@@ -14,6 +19,7 @@ module.exports = function makeFlightPose(flight, fromPosition, toPosition) {
 	let spawnPosition;
 	let spawnOrientation;
 	let spawnVector;
+	let spawnRotationLine;
 	
 	for (const element of flight.elements) {
 		
@@ -33,10 +39,14 @@ module.exports = function makeFlightPose(flight, fromPosition, toPosition) {
 			// Use a random spawn point above airfield
 			if (!spawnPosition || spawnPosition === airfieldPosition) {
 				
+				const offsetX = rand.real(-AIRFIELD_SPAWN_RADIUS, AIRFIELD_SPAWN_RADIUS);
+				const offsetY = rand.real(AIRFIELD_SPAWN_MIN_ALT, AIRFIELD_SPAWN_MAX_ALT);
+				const offsetZ = rand.real(-AIRFIELD_SPAWN_RADIUS, AIRFIELD_SPAWN_RADIUS);
+				
 				spawnPosition = [
-					airfieldPosition[0] + rand.real(-400, 400, true),
-					airfieldPosition[1] + rand.real(250, 350, true),
-					airfieldPosition[2] + rand.real(-400, 400, true)
+					airfieldPosition[0] + offsetX,
+					airfieldPosition[1] + offsetY,
+					airfieldPosition[2] + offsetZ
 				];
 			}
 		}
@@ -71,33 +81,42 @@ module.exports = function makeFlightPose(flight, fromPosition, toPosition) {
 			const diffY = toPosition[1] - spawnPosition[1];
 			const diffZ = toPosition[2] - spawnPosition[2];
 			
-			let orientationY = Math.atan2(diffZ, diffX);
-			let orientationZ = Math.atan2(
+			let yaw = Math.atan2(diffZ, diffX);
+			let pitch = Math.atan2(
 				diffY,
 				Math.sqrt(diffZ * diffZ + diffX * diffX)
 			);
 			
-			orientationY = orientationY * (180 / Math.PI);
-			orientationZ = orientationZ * (180 / Math.PI);
+			yaw = yaw * (180 / Math.PI);
+			pitch = pitch * (180 / Math.PI);
 			
-			if (orientationY < 0) {
-				orientationY += 360;
+			if (yaw < 0) {
+				yaw += 360;
 			}
 			
 			// Apply pitch angle limits
-			orientationZ = Math.max(orientationZ, MIN_PITCH_ANGLE);
-			orientationZ = Math.min(orientationZ, MAX_PITCH_ANGLE);
+			pitch = Math.max(pitch, MIN_PITCH_ANGLE);
+			pitch = Math.min(pitch, MAX_PITCH_ANGLE);
 			
-			if (orientationZ < 0) {
-				orientationZ += 360;
+			if (pitch < 0) {
+				pitch += 360;
 			}
 			
-			spawnOrientation = [0, orientationY, orientationZ];
+			spawnOrientation = [0, yaw, pitch]; // Roll, Yaw, Pitch
 			
 			// Inverted orientation vector (used for formation positioning)
 			spawnVector = Vector.create([diffX, diffY, diffZ])
 				.toUnitVector()
 				.multiply(-1);
+			
+			// Build a perpendicular spawn vector rotation axis line
+			pitch = (pitch + 90) % 360;
+			
+			spawnRotationLine = Line.create(Vector.Zero(3), Vector.create([
+				Math.cos(yaw * (Math.PI / 180)) * Math.cos(pitch * (Math.PI / 180)),
+				Math.sin(pitch * (Math.PI / 180)),
+				Math.sin(yaw * (Math.PI / 180)) * Math.cos(pitch * (Math.PI / 180))
+			]));
 		}
 		
 		// Apply air start orientation and position for each plane
@@ -106,10 +125,12 @@ module.exports = function makeFlightPose(flight, fromPosition, toPosition) {
 			const isElementLeader = (plane === element[0]);
 			const planeItem = plane.item;
 			
+			// Set wingman position based on element formation
 			if (!isElementLeader) {
 				
 				spawnPosition = Vector.create(spawnPosition)
 					.add(spawnVector.multiply(80))
+					.rotate(Math.PI / 2, spawnRotationLine) // FIXME
 					.elements;
 			}
 			
