d4264b1abf825822e049b39c29fff22ea3113be6
Simas Toleikis <simas.toleikis@adform.com>
2016-11-24 17:07:53 +0200

    Create another AI flight for testing.

--- src/make/forces.js
+++ src/make/forces.js
@@ -7,19 +7,29 @@ const makeFlight = require("./flight");
 // Generate mission task forces
 module.exports = function makeForces() {
 
-	const {rand, params, choice} = this;
+	// Make player force
+	makePlayerForce.call(this);
+
+	// FIXME: Make another random AI force for testing
+	makeForce.call(this, {});
+};
+
+// Make a new task force
+function makeForce({player = false, choice = {}, state = 0}) {
+
+	const {rand} = this;
 	const force = [];
+	const flightParams = {};
+	const unit = chooseFlightUnit.call(this, choice);
 	let flight;
 
-	this.forces = [];
+	// Make player flight and task force
+	if (player) {
+		flightParams.player = true;
+	}
 
-	const unit = chooseFlightUnit.call(this, choice);
-
-	const flightParams = {
-		player: true,
-		state: params.state,
-		unit: unit.id
-	};
+	flightParams.unit = unit.id;
+	flightParams.state = state;
 
 	if (choice.task) {
 
@@ -54,61 +64,74 @@ module.exports = function makeForces() {
 
 	// Add flight to task force
 	force.push(flight);
-	this.forces.push(force);
+
+	return force;
+}
+
+// Make player task force
+function makePlayerForce() {
+
+	const {choice, params} = this;
+
+	// Create player task force
+	const force = makeForce.call(this, {
+		player: true,
+		choice,
+		state: params.state
+	});
+
+	const [flight] = force;
 
 	// Set player flight info references
-	if (flight.player) {
+	const player = this.player = Object.create(null);
 
-		const player = this.player = Object.create(null);
+	player.force = force;
+	player.flight = flight;
+	player.plane = flight.player.plane;
+	player.item = flight.player.item;
 
-		player.force = force;
-		player.flight = flight;
-		player.plane = flight.player.plane;
-		player.item = flight.player.item;
+	// Find player element
+	for (const element of flight.elements) {
 
-		// Find player element
-		for (const element of flight.elements) {
+		if (element.player) {
 
-			if (element.player) {
-
-				player.element = element;
-				break;
-			}
+			player.element = element;
+			break;
 		}
-
-		// Log player flight info
-		const logData = ["Flight:"];
-
-		// Log flight unit name
-		const unit = this.units[player.flight.unit];
-		let unitName = unit.name;
-
-		if (unit.suffix) {
-			unitName += " " + unit.suffix;
-		}
-
-		if (unit.alias) {
-			unitName += " “" + unit.alias + "”";
-		}
-
-		logData.push(unitName);
-
-		// Log flight formation and state (for player element)
-		const formation = player.flight.formation;
-		let formationID = formation.id;
-
-		if (!formationID) {
-			formationID = formation.elements.join(":");
-		}
-
-		logData.push({
-			formation: formationID,
-			state: player.element.state
-		});
-
-		log.I.apply(log, logData);
 	}
-};
+
+	// Log player flight info
+	const logData = ["Flight:"];
+
+	// Log flight unit name
+	const unit = this.units[player.flight.unit];
+	let unitName = unit.name;
+
+	if (unit.suffix) {
+		unitName += " " + unit.suffix;
+	}
+
+	if (unit.alias) {
+		unitName += " “" + unit.alias + "”";
+	}
+
+	logData.push(unitName);
+
+	// Log flight formation and state (for player element)
+	const formation = player.flight.formation;
+	let formationID = formation.id;
+
+	if (!formationID) {
+		formationID = formation.elements.join(":");
+	}
+
+	logData.push({
+		formation: formationID,
+		state: player.element.state
+	});
+
+	log.I.apply(log, logData);
+}
 
 // Choose a valid flight unit based on choice data
 function chooseFlightUnit(choice) {
