b2dca7ad6c8b60a6e573ba635856a721bb785f18
Simas Toleikis <simas.toleikis@adform.com>
2018-01-06 10:27:17 +0200

    Apply a limit to number of plane entities.

--- src/make/forces.ai.js
+++ src/make/forces.ai.js
@@ -1,9 +1,9 @@
 import {makeForce} from "./forces"
 
-// NOTE: We have to limit max number of planes even with virtual flights where
-// they exist as disabled entities. It seems disabled entities still consume a
-// lot of memory and have a significant impact on CPU usage.
-const PLANES_MAX_COUNT = 1000
+// NOTE: We have to limit max number of plane entities even with virtual flights
+// It seems disabled entities still consume a lot of memory and have a significant
+// impact on CPU usage.
+const PLANE_ENTITIES_MAX_COUNT = 450
 
 // Percent of serviceable planes available for actual flights.
 // TODO: Make this configurable per battle, coalition and date
@@ -19,24 +19,10 @@ const PLANES_IN_AIR_PERCENT = 0.25 // 25%
 // Make AI task forces
 export default function makeAIForces() {
 
-	/*
-		AI task forces generation algorithm:
-
-			1. Define max planes count for this mission (taking into account available
-				 planes and serviceability).
-			2. Make forces with state=0-1 (in progress) while number of planes in the
-				 air is < PLANES_IN_AIR_PERCENT.
-			3. Advance time and make shedulled forces with state=0 and delay=number
-	*/
-
 	const {rand, availableUnits} = this
 	const activeFlights = []
 
-	const maxPlanesCount = Math.round(Math.min(
-		availableUnits.length * PLANES_SERVICEABILITY_PERCENT,
-		PLANES_MAX_COUNT
-	))
-
+	const maxPlanesCount = Math.round(availableUnits.length * PLANES_SERVICEABILITY_PERCENT)
 	const maxPlanesInAirCount = Math.round(maxPlanesCount * PLANES_IN_AIR_PERCENT)
 
 	// Make initial forces that are in progress and in the air when mission starts
@@ -50,5 +36,8 @@ export default function makeAIForces() {
 		// Track all AI active flights
 		activeFlights.push(...force)
 	}
-	while (this.totalPlanes < maxPlanesInAirCount)
+	while (
+		this.totalPlanes <= maxPlanesInAirCount &&
+		this.totalEntities <= PLANE_ENTITIES_MAX_COUNT
+	)
 }
