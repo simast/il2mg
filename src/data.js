e748bc38034dceae0cc7028d0ce7176a966e820a
Simas Toleikis <simas.toleikis@adform.com>
2018-01-06 11:40:45 +0200

    Updated copyright year.

--- src/data.js
+++ src/data.js
@@ -7,7 +7,7 @@ import addLazyProperty from "lazy-property"
 export const APPLICATION_NAME = "il2mg"
 export const APPLICATION_TITLE = "il2mg - Mission Generator"
 export const APPLICATION_VERSION = "r11"
-export const APPLICATION_COPYRIGHT = "(C) Simas Toleikis, 2014-2017"
+export const APPLICATION_COPYRIGHT = "(C) Simas Toleikis, 2014-2018"
 
 // Coalitions
 export const Coalition = Object.freeze({
