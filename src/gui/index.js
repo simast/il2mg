f56f934e24fb46e24a210e98c952ace258adf89b
Simas Toleikis <simast@gmail.com>
2018-10-28 12:36:13 +0200

    Add missing "include" for tsconfig.json.

--- src/gui/index.js
+++ src/gui/index.js
@@ -1,10 +1,8 @@
 // NOTE: This file is used only in development mode - production index.js file
 // is a bundle built with webpack.
 
-// Enable Babel require() hook
-require('@babel/register')({
-	extends: './.babelrc'
-})
+// Enable ts-node require() hook
+require('ts-node').register()
 
 // Run application logic
 require('./index.jsx')
