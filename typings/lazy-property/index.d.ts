bb3ae26d14530e3be424c413e75e2c493c02d7e2
Simas Toleikis <simast@wix.com>
2019-11-10 20:42:01 +0200

    Reformat custom typings.

--- typings/lazy-property/index.d.ts
+++ typings/lazy-property/index.d.ts
@@ -1,9 +1,8 @@
 declare module 'lazy-property' {
-
 	export default function addLazyProperty<O extends object, P extends keyof O>(
 		obj: O,
 		prop: P,
 		init: (this: O) => O[P],
-		enumerable?: boolean
-	): void
+		enumerable?: boolean,
+	): void;
 }
--- typings/lex/index.d.ts
+++ typings/lex/index.d.ts
@@ -1,8 +1,10 @@
 declare module 'lex' {
-
 	export default class Lexer {
-		addRule(pattern: RegExp, action: (lexeme: string, ...matches: string[]) => void): Lexer
-		setInput(content: string): Lexer
-		lex(): void
+		addRule(
+			pattern: RegExp,
+			action: (lexeme: string, ...matches: string[]) => void,
+		): Lexer;
+		setInput(content: string): Lexer;
+		lex(): void;
 	}
 }
